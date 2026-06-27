0e0d32b318c35ee18aa022c3fbe1409cd1e8b6fe
Mmabatho76 <ST10460268@rcconnect.edu.za>
2026-06-27 20:46:51 +0200

    Add project files.

---
+++ POEPART3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace POEPART3
+{
+    internal class Program
+    {
+       
+            [STAThread]
+            static void Main()
+            {
+                Application.EnableVisualStyles();
+                ApplicatiAon.SetCompatibleTextRenderingDefault(false);
+                ApplicationConfiguration.Initialize();
+                Application.Run(new Form1());
+            }
+        }
+    }
+
+    
